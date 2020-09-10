89792482ca93b45c804edb607f40c075d5e7b792
Denis Kratos <dkratos@shakuro.com>
2020-09-11 00:07:47 +0300

    Add texture

--- features/three/add-box.ts
+++ features/three/add-box.ts
@@ -2,13 +2,15 @@ import {
   Scene,
   BoxGeometry,
   MeshBasicMaterial,
+  TextureLoader,
   Mesh,
 } from 'three';
 
 
 export const addBox = (scene: Scene) => {
   const geometry = new BoxGeometry(2, 2, 2);
-  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+  const texture = new TextureLoader().load('/box.jpg');
+  const material = new MeshBasicMaterial({ map: texture });
   const cube = new Mesh(geometry, material);
 
   scene.add(cube);
